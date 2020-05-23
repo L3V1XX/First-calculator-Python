bb2acfbd355064441428ceb0f3cd01d742dca619
LeviDarkGame <40538162+Trueno14@users.noreply.github.com>
2020-05-23 04:33:54 +0200

    Calculador V1
    - It now has a different visual appearance.
    - It's better optimized

--- Calculadora.py
+++ Calculadora.py
@@ -5,16 +5,23 @@ expression = ""
 
 root = Tk()
 root.title("Calculadora Levi")
-root.geometry("304x155")
+root.geometry("400x600")
+root.resizable(False,False)
+root.configure(background = "gray42")
+
+# Caracteristicas para los botones
+
+color_boton = "gray99"
+width_boton = 10
+high_boton = 3
+
+
 
 equation = StringVar()
 
-display = Entry(root, textvariable = equation)
+display = Entry(root,font = ("arial",20,"bold"),width = 22,borderwidth = 10, background = "Gray100", textvariable = equation)
 display.place (height = 90)
-display.grid(row = 1 , columnspan= 6, ipadx = 90, ipady = 3,sticky = W+E)
-
-equation.set ("")
-
+display.grid(row = 0,column = 0,columnspan = 4 ,padx = 25, pady = 20)
 
 i = 0
 
@@ -65,41 +72,50 @@ def calculate():
         clear_display()
         display.insert(0,'Error')
 
+
+def clear():
+    global operator
+    operator = ""
+    equation.set("0")
+
+clear()
+
 # Numeros y botones
 
-Button(root, text ="1", command = lambda:get_numbers(1)).grid(row = 2,column = 0,sticky = W+E,)
-Button(root, text ="2",command = lambda:get_numbers(2)).grid(row = 2, column = 1,sticky = W+E)
-Button(root, text ="3",command = lambda:get_numbers(3)).grid(row = 2, column = 2,sticky = W+E)
+Button(root, text ="1",bg = color_boton, width = width_boton, height = high_boton, command = lambda:get_numbers(1)).grid(row = 2,column = 0,pady = 10)
+Button(root, text ="2",bg = color_boton, width = width_boton, height = high_boton, command = lambda:get_numbers(2)).grid(row = 2,column = 1,pady = 10)
+Button(root, text ="3",bg = color_boton, width = width_boton, height = high_boton, command = lambda:get_numbers(3)).grid(row = 2,column = 2,pady = 10)
 
-Button(root, text ="4",command = lambda:get_numbers(4)).grid(row = 3, column = 0,sticky = W+E)
-Button(root, text ="5",command = lambda:get_numbers(5)).grid(row = 3, column = 1,sticky = W+E)
-Button(root, text ="6",command = lambda:get_numbers(6)).grid(row = 3, column = 2,sticky = W+E)
+Button(root, text ="4",bg = color_boton, width = width_boton, height = high_boton, command = lambda:get_numbers(4)).grid(row = 3,column = 0,pady = 10)
+Button(root, text ="5",bg = color_boton, width = width_boton, height = high_boton, command = lambda:get_numbers(5)).grid(row = 3,column = 1,pady = 10)
+Button(root, text ="6",bg = color_boton, width = width_boton, height = high_boton, command = lambda:get_numbers(6)).grid(row = 3,column = 2,pady = 10)
 
-Button(root, text ="7",command = lambda:get_numbers(7)).grid(row = 4, column = 0,sticky = W+E)
-Button(root, text ="8",command = lambda:get_numbers(8)).grid(row = 4, column = 1,sticky = W+E)
-Button(root, text ="9",command = lambda:get_numbers(9)).grid(row = 4, column = 2,sticky = W+E)
+Button(root, text ="7",bg = color_boton, width = width_boton, height = high_boton, command = lambda:get_numbers(7)).grid(row = 4, column = 0,pady = 10)
+Button(root, text ="8",bg = color_boton, width = width_boton, height = high_boton, command = lambda:get_numbers(8)).grid(row = 4, column = 1,pady = 10)
+Button(root, text ="9",bg = color_boton, width = width_boton, height = high_boton, command = lambda:get_numbers(9)).grid(row = 4, column = 2,pady = 10)
 
 # Botones Numericos
 
-Button(root, text ="AC",command = lambda: clear_display()).grid(row = 5, column = 0,sticky = W+E)
-Button(root, text ="0", command  = lambda:get_numbers(0)).grid(row = 5, column = 1,sticky = W+E)
-Button(root, text ="%", command = lambda:get_operation("%")).grid(row = 5, column = 2,sticky = W+E)
+Button(root, text ="AC",bg = color_boton, width = width_boton, height = high_boton, command = lambda: clear_display()).grid(row = 5, column = 0,pady = 10)
+Button(root, text ="0",bg = color_boton, width = width_boton, height = high_boton, command = lambda:get_numbers(0)).grid(row = 5, column = 1,pady = 10)
+Button(root, text ="%",bg = color_boton, width = width_boton, height = high_boton, command = lambda:get_operation("%")).grid(row = 5, column = 2,pady =10)
 
 # Botones para ejercer las operaciones matematicas
 
-Button(root, text = "+", command = lambda:get_operation("+")).grid(row= 2, column = 3,sticky = W+E)
-Button(root, text = "-", command = lambda:get_operation("-")).grid(row= 3, column = 3,sticky = W+E)
-Button(root, text = "*", command = lambda:get_operation("*")).grid(row= 4, column = 3,sticky = W+E)
-Button(root, text = "/", command = lambda:get_operation("/")).grid(row= 5, column = 3,sticky = W+E)
+Button(root, text = "+",bg = color_boton, width = width_boton, height = high_boton, command = lambda:get_operation("+")).grid(row= 6, column = 0,pady = 10)
+Button(root, text = "-",bg = color_boton, width = width_boton, height = high_boton, command = lambda:get_operation("-")).grid(row= 6, column = 1,pady = 10)
+Button(root, text = "*",bg = color_boton, width = width_boton, height = high_boton, command = lambda:get_operation("*")).grid(row= 6, column = 2,pady = 10)
+Button(root, text = "/",fg = "white", bg = "black" , width = width_boton, height = high_boton, command = lambda:get_operation("/")).grid(row= 6, column = 3,pady = 10)
 
-Button(root, text = "⌫",fg = "white", bg = "black", bd = 1.2 , command = lambda: undo()).grid(row= 2, column = 4,sticky = W+E,columnspan = 2)
-Button(root, text = "exp",fg = "white", bg = "black", bd = 1.2 , command = lambda:get_operation("**")).grid(row= 3,column = 4,sticky = W+E)
-Button(root, text = "^2",fg = "white", bg = "black", bd = 1.2 , command = lambda:get_operation("**2")).grid(row= 3, column = 5,sticky = W+E)
-Button(root, text = "(",fg = "white", bg = "black", bd = 1.2 , command = lambda:get_operation("(")).grid(row= 4, column = 4,sticky = W+E)
-Button(root, text = ")",fg = "white", bg = "black", bd = 1.2 , command = lambda:get_operation(")")).grid(row= 4, column = 5,sticky = W+E)
-Button(root, text = "[",fg = "white", bg = "black", bd = 1.2 , command = lambda:get_operation("[")).grid(row= 5, column = 4,sticky = W+E)
-Button(root, text = "]",fg = "white", bg = "black", bd = 1.2 , command = lambda:get_operation("]")).grid(row= 5, column = 5,sticky = W+E)
-Button(root, text = "=",fg = "white", bg = "black", bd = 1.2 , command = lambda:calculate()).grid(row= 6, column = 0 ,sticky = W+E, columnspan = 6)
+Button(root, text = "⌫",fg = "white", bg = "black" , width = width_boton, height = high_boton, command = lambda: undo()).grid(row= 2, column = 3,columnspan = 2, pady = 10)
+Button(root, text = "exp",fg = "white", bg = "black" , width = width_boton, height = high_boton, command = lambda:get_operation("**")).grid(row= 3,column = 3,pady = 10)
+Button(root, text = "^2",fg = "white", bg = "black" , width = width_boton, height = high_boton, command =lambda:get_operation("**2")).grid(row= 4, column = 3,pady = 10)
+Button(root, text = "√",fg = "white", bg = "black" , width = width_boton, height = high_boton, command =lambda:get_operation("sqrt")).grid(row= 5, column = 3,pady = 10)
+Button(root, text = "(",fg = "white", bg = "black" , width = width_boton, height = high_boton, command = lambda:get_operation("(")).grid(row= 7, column = 0,pady = 10)
+Button(root, text = ")",fg = "white", bg = "black" , width = width_boton, height = high_boton, command = lambda:get_operation(")")).grid(row= 7, column = 1,pady = 10)
+Button(root, text = "[",fg = "white", bg = "black" , width = width_boton, height = high_boton, command = lambda:get_operation("[")).grid(row= 7, column = 2,pady = 10)
+Button(root, text = "]",fg = "white", bg = "black" , width = width_boton, height = high_boton, command = lambda:get_operation("]")).grid(row= 7, column = 3, pady = 10)
+Button(root, text = "=",fg = "white", bg = "black", width = width_boton, height = high_boton, command = lambda:calculate()).grid(row= 8, column = 0, sticky = W+E, columnspan = 5)
 
 
 root.mainloop()
